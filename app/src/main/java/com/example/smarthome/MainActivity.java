b53960110f797374478ac96866e0b87b6365ea65
DS-ann <dsannigrahi5956@gmail.com>
2026-05-07 07:41:18 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/smarthome/MainActivity.java
+++ app/src/main/java/com/example/smarthome/MainActivity.java
@@ -1,1 +1,34 @@
+package com.example.smarthome;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings ws = webView.getSettings();
+
+        ws.setJavaScriptEnabled(true);
+        ws.setDomStorageEnabled(true);
+
+        webView.addJavascriptInterface(
+            new BLEBridge(this),
+            "Android"
+        );
+
+        webView.loadUrl(
+            "file:///android_asset/index.html"
+        );
+    }
+}
