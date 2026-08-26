3ec15422039ef595604c6d1e584c196214116979
DS-ann <dsannigrahi5956@gmail.com>
2026-08-26 08:30:58 +0530

    Handle widget commands through existing BLE bridge

--- app/src/main/java/com/example/smarthome/MainActivity.java
+++ app/src/main/java/com/example/smarthome/MainActivity.java
@@ -6,17 +6,23 @@ import android.os.Build;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int BLE_PERMISSION_REQUEST = 1001;
+    private static final String EXTRA_COMMAND = "command";
     private WebView webView;
+    private BLEBridge bleBridge;
+    private String pendingWidgetCommand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        pendingWidgetCommand = getIntent().getStringExtra(EXTRA_COMMAND);
 
         webView = new WebView(this);
         setContentView(webView);
@@ -27,10 +33,27 @@ public class MainActivity extends AppCompatActivity {
         ws.setAllowFileAccess(true);
         ws.setAllowContentAccess(true);
 
-        webView.addJavascriptInterface(
-                new BLEBridge(this, webView),
-                "Android"
-        );
+        bleBridge = new BLEBridge(this, webView);
+        webView.addJavascriptInterface(bleBridge, "Android");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                if (pendingWidgetCommand != null) {
+                    final String command = pendingWidgetCommand;
+                    pendingWidgetCommand = null;
+                    view.postDelayed(new Runnable() {
+                        @Override public void run() {
+                            bleBridge.connectBLE();
+                            view.postDelayed(new Runnable() {
+                                @Override public void run() {
+                                    bleBridge.sendBLE(command);
+                                }
+                            }, 700);
+                        }
+                    }, 250);
+                }
+            }
+        });
 
         webView.loadUrl("file:///android_asset/index.html");
         requestBlePermissionsIfNeeded();
